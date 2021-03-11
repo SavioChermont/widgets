d8429d244d2825f9ce7333dfdb9fc34296eb7206
SavioChermont <saviochwt@gmail.com>
2021-03-11 17:50:55 -0300

    Cria consulta na API da wikipedia e resultados na tela

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,37 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState(" ");
+  const [results, setResults] = useState([""]);
 
   useEffect(() => {
-    console.log(term);
+    (async () => {
+      if (term) {
+        const { data } = await axios.get("http://en.wikipedia.org/w/api.php", {
+          params: {
+            action: "query",
+            list: "search",
+            origin: "*",
+            format: "json",
+            srsearch: term,
+          },
+        });
+
+        setResults(data.query.search);
+      }
+    })();
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item">
+        <div className="content">
+          <div className="header"> {result.title} </div>
+          {result.sinppet}
+        </div>
+      </div>
+    );
   });
 
   return (
@@ -19,6 +46,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
